42f66c0b03c3855dec7fc3ed32e7d528f1ff83dd
Susan K <kralsusa@yahoo.com>
2019-07-16 11:50:20 -0700

    added parts of the quiz

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-/* eslint-disable no-console */ 
-import isYes from .src/isYes";
+
+import isYes from './src/isYes.js';
+
+
+const ButtonInput = document.getElementById('quizButton');
+const result = document.getElementById('quizResult')
+
+ButtonInput.onclick = function(){
+
+    var nameInput = prompt 'Please enter your name';
+    const confirmed = confirm(nameInput + ', are you sure you want to take a quiz?') 
+    if (confirmed === false) {return;} ;
+
+//Actual quiz questions
+
+import isYes from './src/isYes.js';
+
+   
