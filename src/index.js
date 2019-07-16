52b9f44f35fc46ef754f1e76717c14b427214057
Susan K <kralsusa@yahoo.com>
2019-07-16 13:01:29 -0700

    connected html to java

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 
 import isYes from './src/isYes.js';
 
-
-const ButtonInput = document.getElementById('quizButton');
+const buttonInput = document.getElementById('quiz-button');
 const result = document.getElementById('quizResult')
 
-ButtonInput.onclick = function(){
+buttonInput.onclick = function(){
 
-    var nameInput = prompt 'Please enter your name';
+    var nameInput = prompt('Please enter your name');
     const confirmed = confirm(nameInput + ', are you sure you want to take a quiz?') 
-    if (confirmed === false) {return;} ;
+    if (confirmed === false) {return;};
 
 //Actual quiz questions
 
-import isYes from './src/isYes.js';
+   const inAdults = prompt('Does neurogenesis happen in adults (Y/N)');
+   //Assess with ifYes
+   const correctinAdults = true; 
+    if(isYes(inAdults) ===correctinAdults) {
+        correctAnswers +=1;
+    }
 
-   
+   const inHippocampus = prompt('Does neurogenesis happen in the hippocampus (Y/N)');
+   const correctinHippocampus = true; 
+   if(isYes(inHippocampus) ===correctinHippocampus) {
+       correctAnswers +=1;
+   }
+
+   const inHumans = prompt('Does neurogenesis happen in humans (Y/N)');
+   const correctinHumans = true; 
+   if(isYes(inHumans) ===correctinHumans) {
+       correctAnswers +=1;
+   }
+
+   const totalScore = correctinAdults + correctinHippocampus + correctinHumans;
