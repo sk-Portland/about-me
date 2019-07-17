dcb1b8ecc17c90fef588fa8e85f419ffcfb1aee1
Susan K <kralsusa@yahoo.com>
2019-07-17 11:40:47 -0700

    code some js code for the webpage

--- src/number-tries.js
+++ src/number-tries.js
@@ -1,3 +1,22 @@
-cost numberTries;
-for (numberTries = 4; numberTries-) 
+
+  import calculateNumber from '../src/guess-number.js'
+
+ const guessNumber = document.getElementById("guessNumber");
+  
+  var givenNumber=15;
+ 
+   function(calculateNumber){givenNumber, guessNumber};
+
+
+
+
+ const numberTries;
+for (numberTries = 4; numberTries > 0; numberTries--) 
     {console.log('These are the number of tries you have left:' + numberTries);
+
+
+
+
+  
+
+}
